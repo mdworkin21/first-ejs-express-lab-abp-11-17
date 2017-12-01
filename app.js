e9d2e850efbc9c23c3e4a3bbfdc4ef0090c3703d
Matthew Dworkin <mdworkin21@gmail.com>
2017-12-01 20:42:57 +0000

    automatically backed up by learn

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+app.get(`/`, function(req, resp){
+    resp.render("index", {welcomePhrase: "Hello, World!"})
+
+})
 
 
 module.exports = app
